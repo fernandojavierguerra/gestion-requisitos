fe7b6e77cbb9ab5406a78b5e8c48f0bd1eaf56fd
Javier Guerra <fernandojavierguerra@gmail.com>
2017-06-16 15:56:15 -0300

    Main

--- src/main/java/com/durgam/guerra/GestionRequisitosApplication.java
+++ src/main/java/com/durgam/guerra/GestionRequisitosApplication.java
@@ -3,12 +3,15 @@ package com.durgam.guerra;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.durgam.guerra.dominio.GestionRequisito;
+
 @SpringBootApplication
 public class GestionRequisitosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestionRequisitosApplication.class, args);
 	
-		
+		GestionRequisito app= GestionRequisito.getSistema();
+		System.out.println("Creando Objeto Aplicación en la Base de Datos");
 	}
 }
