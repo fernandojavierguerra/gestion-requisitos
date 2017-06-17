c692f73b09d17a610fdd3a5037543b5879790669
Javier Guerra <fernandojavierguerra@gmail.com>
2017-06-17 10:02:37 -0300

    Transaccional

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,15 +1,14 @@
-INSERT INTO GestionRequisito (id, nombre) VALUES (1,'Sistema de Gestion de Requisitos');
+-- Dumping data for table `gestion_requisito`
 
-
+INSERT INTO `gestion_requisito` (`id`, `nombre`) VALUES (1,'Sistema Requisitos 2017');
 
 
 --
 -- Dumping data for table `proyecto`
 --
 
-INSERT INTO Proyecto (id, descripcionProyecto, nombreProyecto, aplicacion_id) VALUES (1,'Cambio de version de BBDD','Proyecto 1',1);
-INSERT INTO Proyecto (id, descripcionProyecto, nombreProyecto, aplicacion_id) VALUES (2,'Proyecto Semaforos','Proyecto 2',1);
-INSERT INTO Proyecto (id, descripcionProyecto, nombreProyecto, aplicacion_id) VALUES (3,'Sistema Contable','Proyecto 3',1);
-INSERT INTO Proyecto (id, descripcionProyecto, nombreProyecto, aplicacion_id) VALUES (4,'Cambio de version de BBDD','Proyecto 4',1);
-
-
+/*!40000 ALTER TABLE `proyecto` DISABLE KEYS */;
+INSERT INTO `proyecto` (`id`, `descripcion_proyecto`, `nombre_proyecto`, `aplicacion_id`) VALUES (1,'Cambio de version de BBDD','Proyecto 1',1);
+INSERT INTO `proyecto` (`id`, `descripcion_proyecto`, `nombre_proyecto`, `aplicacion_id`) VALUES (2,'Proyecto Semaforos','Proyecto 2',1);
+INSERT INTO `proyecto` (`id`, `descripcion_proyecto`, `nombre_proyecto`, `aplicacion_id`) VALUES (3,'Sistema Contable','Proyecto 3',1);
+INSERT INTO `proyecto` (`id`, `descripcion_proyecto`, `nombre_proyecto`, `aplicacion_id`) VALUES (4,'Cambio de version de BBDD','Proyecto 4',1);
