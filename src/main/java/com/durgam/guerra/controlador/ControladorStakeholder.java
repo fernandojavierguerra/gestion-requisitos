0a2ffd63990a721e0e456ae35352f868a003512f
fernandojavierguerra <fernandojavierguerra@github.com>
2017-06-30 21:17:59 -0300

    Quitar requisito a un stakeholder

--- src/main/java/com/durgam/guerra/controlador/ControladorStakeholder.java
+++ src/main/java/com/durgam/guerra/controlador/ControladorStakeholder.java
@@ -7,12 +7,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import com.durgam.guerra.dominio.Stakeholder;
+import com.durgam.guerra.servicio.ServicioRequisito;
 import com.durgam.guerra.servicio.ServicioStakeholder;
 
 @Controller
 public class ControladorStakeholder {
 	@Autowired
 	private ServicioStakeholder servicioStakeholder;
+	
+	@Autowired
+	private ServicioRequisito servicioRequisito;
 	 
 	@RequestMapping(value = "/stakeholders", method = RequestMethod.GET)
 	    public String list(Model model){
@@ -45,6 +49,12 @@ public class ControladorStakeholder {
 	 @RequestMapping("stakeholder/delete/{id}")
 	 public String delete(@PathVariable Long id){
 		 servicioStakeholder.borrarStakeholderId(id);
+	     return "redirect:/stakeholders";
+	 }	
+	 
+	 @RequestMapping("/stakeholder/quitar/{id}/{id_requisito}")
+	 public String quitarRequisito(@PathVariable Long id, Long id_requisito){
+		 servicioStakeholder.quitarRequisitoStakeholder(servicioStakeholder.buscarStakeholderPorId(id), servicioRequisito.buscarRequisitoPorId(id_requisito));
 	     return "redirect:/stakeholders";
 	 }	
 }
